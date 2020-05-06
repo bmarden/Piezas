27432d0fd894c9ad0736956774ba2204477c060b
Ben Marden <benvmarden@gmail.com>
2020-05-05 17:42:23 -0700

    Separate test cases

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -14,21 +14,39 @@ class PiezasTest : public ::testing::Test {
     virtual void TearDown() {}  //clean up after each test, (before destructor)
 };
 
-TEST(PiezasTest, pieceAtOutOfRange) {
+TEST(PiezasTest, pieceAtColOutOfRangePositive) {
     Piezas newGame;
-
-    // Testing multiple out of range possibilties. All should return Invalid
     ASSERT_EQ(newGame.pieceAt(2, 4), Invalid);
+}
+
+TEST(PiezasTest, pieceAtRowOutOfRangePositive) {
+    Piezas newGame;
     ASSERT_EQ(newGame.pieceAt(3, 2), Invalid);
+}
+
+TEST(PiezasTest, pieceAtColOutOfRangeNegative) {
+    Piezas newGame;
+    ASSERT_EQ(newGame.pieceAt(3, -1), Invalid);
+}
+
+TEST(PiezasTest, pieceAtRowOutOfRangeNegative) {
+    Piezas newGame;
     ASSERT_EQ(newGame.pieceAt(-1, 2), Invalid);
 }
 
-TEST(PiezasTest, pieceAtReturnValue) {
+TEST(PiezasTest, pieceAtReturnBlank1) {
     Piezas newGame;
-
     ASSERT_EQ(newGame.pieceAt(2, 3), Blank);
+}
+
+TEST(PiezasTest, pieceAtReturnBlank2) {
+    Piezas newGame;
+    ASSERT_EQ(newGame.pieceAt(2, 0), Blank);
+}
+
+TEST(PiezasTest, pieceAtReturnBlank3) {
+    Piezas newGame;
     ASSERT_EQ(newGame.pieceAt(0, 0), Blank);
-    ASSERT_EQ(newGame.pieceAt(0, 3), Blank);
 }
 
 TEST(PiezasTest, resetBoard) {
@@ -41,35 +59,53 @@ TEST(PiezasTest, resetBoard) {
     }
 }
 
+TEST(PiezasTest, dropPieceExpectO) {
+    Piezas newGame;
+    EXPECT_EQ(newGame.dropPiece(0), O);
+}
+
+TEST(PiezasTest, dropPieceExpectX) {
+    Piezas newGame;
+    newGame.dropPiece(1);
+    EXPECT_EQ(newGame.dropPiece(2), X);
+}
+
+TEST(PiezasTest, dropPieceExpectO1) {
+    Piezas newGame;
+    newGame.dropPiece(3);
+    newGame.dropPiece(3);
+    EXPECT_EQ(newGame.dropPiece(2), O);
+}
+
 TEST(PiezasTest, dropPieceExpectedTurn) {
     Piezas newGame;
-
-    ASSERT_EQ(newGame.dropPiece(0), O);
-    ASSERT_EQ(newGame.dropPiece(2), X);
-    ASSERT_EQ(newGame.dropPiece(2), O);
-    ASSERT_EQ(newGame.dropPiece(3), X);
+    EXPECT_EQ(newGame.dropPiece(3), O);
 }
 
 TEST(PiezasTest, dropPieceInvalidColumn) {
     Piezas newGame;
-
     ASSERT_EQ(newGame.dropPiece(4), Invalid);
-    ASSERT_EQ(newGame.dropPiece(5), Invalid);
-    ASSERT_EQ(newGame.dropPiece(-1), Invalid);
     ASSERT_EQ(newGame.dropPiece(100), Invalid);
 }
 
 TEST(PiezasTest, dropPieceFullColumn) {
     Piezas newGame;
-
     newGame.dropPiece(0);
     newGame.dropPiece(0);
     newGame.dropPiece(0);
     ASSERT_EQ(newGame.dropPiece(0), Blank);
+}
+
+TEST(PiezasTest, dropPieceFullCol1) {
+    Piezas newGame;
     newGame.dropPiece(1);
     newGame.dropPiece(1);
     newGame.dropPiece(1);
     ASSERT_EQ(newGame.dropPiece(1), Blank);
+}
+
+TEST(PiezasTest, dropPieceFullCol2) {
+    Piezas newGame;
     newGame.dropPiece(2);
     newGame.dropPiece(2);
     newGame.dropPiece(2);
