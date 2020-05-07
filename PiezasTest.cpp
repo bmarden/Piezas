ae83ef49c30179539fa08639b75fb9401f180cd5
Ben Marden <benvmarden@gmail.com>
2020-05-06 19:08:03 -0700

    Additional tests created for gameState

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -15,122 +15,214 @@ class PiezasTest : public ::testing::Test {
 };
 
 TEST(PiezasTest, pieceAtColOutOfRangePositive) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.pieceAt(2, 4), Invalid);
+    Piezas p;
+    ASSERT_EQ(p.pieceAt(2, 4), Invalid);
 }
 
 TEST(PiezasTest, pieceAtRowOutOfRangePositive) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.pieceAt(3, 2), Invalid);
+    Piezas p;
+    ASSERT_EQ(p.pieceAt(3, 2), Invalid);
 }
 
 TEST(PiezasTest, pieceAtColOutOfRangeNegative) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.pieceAt(3, -1), Invalid);
+    Piezas p;
+    ASSERT_EQ(p.pieceAt(3, -1), Invalid);
 }
 
 TEST(PiezasTest, pieceAtRowOutOfRangeNegative) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.pieceAt(-1, 2), Invalid);
+    Piezas p;
+    ASSERT_EQ(p.pieceAt(-1, 2), Invalid);
 }
 
 TEST(PiezasTest, pieceAtReturnBlank1) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.pieceAt(2, 3), Blank);
+    Piezas p;
+    ASSERT_EQ(p.pieceAt(2, 3), Blank);
 }
 
 TEST(PiezasTest, pieceAtReturnBlank2) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.pieceAt(2, 0), Blank);
+    Piezas p;
+    ASSERT_EQ(p.pieceAt(2, 0), Blank);
 }
 
 TEST(PiezasTest, pieceAtReturnBlank3) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.pieceAt(0, 0), Blank);
+    Piezas p;
+    ASSERT_EQ(p.pieceAt(0, 0), Blank);
 }
 
 TEST(PiezasTest, resetBoard) {
-    Piezas newGame;
-    newGame.reset();
+    Piezas p;
+    p.reset();
     for (size_t i = 0; i < BOARD_ROWS; i++) {
         for (size_t j = 0; j < BOARD_COLS; j++) {
-            ASSERT_EQ(newGame.pieceAt(i, j), Blank);
+            ASSERT_EQ(p.pieceAt(i, j), Blank);
         }
     }
 }
 
 TEST(PiezasTest, dropPieceExpectO) {
-    Piezas newGame;
-    EXPECT_EQ(newGame.dropPiece(0), O);
+    Piezas p;
+    EXPECT_EQ(p.dropPiece(0), O);
 }
 
 TEST(PiezasTest, dropPieceExpectX) {
-    Piezas newGame;
-    newGame.dropPiece(1);
-    EXPECT_EQ(newGame.dropPiece(2), X);
+    Piezas p;
+    p.dropPiece(1);
+    EXPECT_EQ(p.dropPiece(2), X);
 }
 
 TEST(PiezasTest, dropPieceExpectO1) {
-    Piezas newGame;
-    newGame.dropPiece(3);
-    newGame.dropPiece(3);
-    EXPECT_EQ(newGame.dropPiece(2), O);
+    Piezas p;
+    p.dropPiece(3);
+    p.dropPiece(3);
+    EXPECT_EQ(p.dropPiece(2), O);
 }
 
 TEST(PiezasTest, dropPieceExpectedTurn) {
-    Piezas newGame;
-    EXPECT_EQ(newGame.dropPiece(3), O);
+    Piezas p;
+    EXPECT_EQ(p.dropPiece(3), O);
 }
 
 TEST(PiezasTest, dropPieceInvalidColumn) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.dropPiece(4), Invalid);
-    ASSERT_EQ(newGame.dropPiece(100), Invalid);
+    Piezas p;
+    ASSERT_EQ(p.dropPiece(4), Invalid);
+    ASSERT_EQ(p.dropPiece(100), Invalid);
 }
 
 TEST(PiezasTest, dropPieceFullColumn) {
-    Piezas newGame;
-    newGame.dropPiece(0);
-    newGame.dropPiece(0);
-    newGame.dropPiece(0);
-    ASSERT_EQ(newGame.dropPiece(0), Blank);
+    Piezas p;
+    p.dropPiece(0);
+    p.dropPiece(0);
+    p.dropPiece(0);
+    ASSERT_EQ(p.dropPiece(0), Blank);
 }
 
 TEST(PiezasTest, dropPieceFullCol1) {
-    Piezas newGame;
-    newGame.dropPiece(1);
-    newGame.dropPiece(1);
-    newGame.dropPiece(1);
-    ASSERT_EQ(newGame.dropPiece(1), Blank);
+    Piezas p;
+    p.dropPiece(1);
+    p.dropPiece(1);
+    p.dropPiece(1);
+    ASSERT_EQ(p.dropPiece(1), Blank);
 }
 
 TEST(PiezasTest, dropPieceFullCol2) {
-    Piezas newGame;
-    newGame.dropPiece(2);
-    newGame.dropPiece(2);
-    newGame.dropPiece(2);
-    ASSERT_EQ(newGame.dropPiece(2), Blank);
+    Piezas p;
+    p.dropPiece(2);
+    p.dropPiece(2);
+    p.dropPiece(2);
+    ASSERT_EQ(p.dropPiece(2), Blank);
+}
+
+TEST(PiezasTest, dropPieceColValues) {
+    Piezas p;
+    p.dropPiece(0);
+    p.dropPiece(0);
+    p.dropPiece(0);
+    EXPECT_EQ((char)p.pieceAt(0, 0), (char)X);
+    EXPECT_EQ(p.pieceAt(1, 0), O);
+    EXPECT_EQ(p.pieceAt(2, 0), X);
 }
 
 TEST(PiezasTest, gameStateGameIncomplete) {
-    Piezas newGame;
-    ASSERT_EQ(newGame.gameState(), Invalid);
+    Piezas p;
+    // Game Board
+    /** 
+     * 0 1 2 3
+     * * * * *
+     * * * * *
+     * * * * * 
+    **/
+    ASSERT_EQ(p.gameState(), Invalid);
 }
 
-TEST(PiezasTest, gameStateGameXWins) {
-    Piezas newGame;
-    newGame.dropPiece(0);  // X
-    newGame.dropPiece(1);  // O
-    newGame.dropPiece(0);  // X
-    newGame.dropPiece(2);  // O
-    newGame.dropPiece(0);  // X
-    newGame.dropPiece(2);  // X
-    newGame.dropPiece(1);  // O
-    newGame.dropPiece(1);  // X
-    newGame.dropPiece(2);  // O
-    newGame.dropPiece(2);  // X
-    newGame.dropPiece(3);  // O
-    newGame.dropPiece(3);  // X
-    newGame.dropPiece(3);  // O
-    ASSERT_EQ(newGame.gameState(), Invalid);
+TEST(PiezasTest, gameStateGameIncomplete2) {
+    Piezas p;
+
+    // Game Board
+    /** 
+     * 0 1 2 3
+     * * * * *
+     * * * * *
+     * X O X * 
+    **/
+    p.dropPiece(0);  // X
+    p.dropPiece(1);  // O
+    p.dropPiece(2);  // X
+    ASSERT_EQ(p.gameState(), Invalid);
+}
+
+TEST(PiezasTest, gameStateXWinsbyRows) {
+    Piezas p;
+
+    // Game Board
+    /** 
+     * 0 1 2 3
+     * O O X O
+     * O O X O
+     * X X X X 
+    **/
+    p.dropPiece(0);  // X
+    p.dropPiece(0);  // O
+    p.dropPiece(1);  // X
+    p.dropPiece(0);  // O
+    p.dropPiece(2);  // X
+    p.dropPiece(1);  // O
+    p.dropPiece(3);  // X
+    p.dropPiece(1);  // O
+    p.dropPiece(2);  // X
+    p.dropPiece(3);  // O
+    p.dropPiece(2);  // X
+    p.dropPiece(3);  // O
+    ASSERT_EQ((char)p.gameState(), (char)X);
+}
+
+TEST(PiezasTest, gameStateOWinsbyRows) {
+    Piezas p;
+
+    // Game Board
+    /** 
+     * 0 1 2 3
+     * O O O O
+     * X O X X
+     * X O X X 
+    **/
+    p.dropPiece(0);  // X
+    p.dropPiece(1);  // O
+    p.dropPiece(2);  // X
+    p.dropPiece(1);  // O
+    p.dropPiece(3);  // X
+    p.dropPiece(1);  // O
+    p.dropPiece(0);  // X
+    p.dropPiece(0);  // O
+    p.dropPiece(2);  // X
+    p.dropPiece(2);  // O
+    p.dropPiece(3);  // X
+    p.dropPiece(3);  // O
+
+    ASSERT_EQ((char)p.gameState(), (char)O);
+}
+
+TEST(PiezasTest, gameStateXWinsbyCols) {
+    Piezas p;
+    // Game Board
+    /** 
+     * 0 1 2 3
+     * O X X O
+     * O X O X
+     * X X O O 
+    **/
+
+    p.dropPiece(0);  // X
+    p.dropPiece(0);  // O
+    p.dropPiece(1);  // X
+    p.dropPiece(0);  // O
+    p.dropPiece(1);  // X
+    p.dropPiece(2);  // O
+    p.dropPiece(1);  // X
+    p.dropPiece(2);  // O
+    p.dropPiece(2);  // X
+    p.dropPiece(3);  // O
+    p.dropPiece(3);  // X
+    p.dropPiece(3);  // O
+
+    ASSERT_EQ((char)p.gameState(), (char)X);
 }
